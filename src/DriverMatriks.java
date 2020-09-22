fce5569099943d1c2fa7676e68e384432ab7c54b
reymyr <reyhan_emyr@hotmail.com>
2020-09-22 23:52:56 +0700

    add determinant with cofactor & update constructor

--- src/DriverMatriks.java
+++ src/DriverMatriks.java
@@ -2,13 +2,26 @@ import java.util.Scanner;
 
 public class DriverMatriks {
     public static void main(String[] args) {
-        int m, n;
+        int m, n, i, j;
         Scanner in = new Scanner(System.in);
         System.out.print("m: ");
         m = in.nextInt();
         System.out.print("n: ");
         n = in.nextInt();
-        Matriks mat = new Matriks(m, n);
+        float[][] A = new float[m][n+1];
+        for(i = 0; i < m; i++) {
+            for (j = 0; j < n+1; j++) {
+                if (j != n) {
+                    System.out.print("Koefisien a"+(i+1)+(j+1)+": ");
+                }
+                else {
+                    System.out.print("Konstanta b"+(i+1)+": ");
+                }
+                A[i][j] = in.nextFloat();
+            }
+        }
+        Matriks mat = new Matriks(A);
         mat.printMatriks();
+        in.close();
     }
 }
--- src/Matriks.java
+++ src/Matriks.java
@@ -1,29 +1,45 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Matriks {
     int M;
     int N;
     float[][] Mat;
 
-    // Membentuk matriks augmented dari SPL dengan input m dan n
+    // Membentuk matriks kosong berukuran MxN
     public Matriks(int M, int N) { 
         int i, j;
         this.M = M;
         this.N = N+1;
-        Scanner in = new Scanner(System.in);
         this.Mat = new float[M][N+1];
         for(i = 0; i < M; i++) {
-            for (j = 0; j < N+1; j++) {
-                if (j != N) {
-                    System.out.print("Koefisien a"+(i+1)+(j+1)+": ");
-                }
-                else {
-                    System.out.print("Konstanta b"+(i+1)+": ");
-                }
+            for (j = 0; j < N; j++) {
+                this.Mat[i][j] = 0;
+            }
+        }
+    }
+
+    // Membaca matriks dari sebuah file
+    public Matriks(File f) {
+
+    }
+
+    // Membentuk matriks dari array 2 dimensi
+    public Matriks(float[][] A) {
+        this.M = A.length;
+        this.N = A[0].length;
+        this.Mat = A;
+    }
+
+    // Menerima input matriks dari pengguna
+    public void inputMatriks() {
+        int i, j;
+        Scanner in = new Scanner(System.in);
+        for(i = 0; i < M; i++) {
+            for (j = 0; j < N; j++) {
                 this.Mat[i][j] = in.nextFloat();
             }
         }
-        
     }
 
     // Menuliskan matriks
@@ -34,6 +50,60 @@ public class Matriks {
                 System.out.print(Mat[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    public void gauss() {
+
+    }
+    public void gaussJordan() {
+
+    }
+
+    // Menghitung determinan matriks dengan metode reduksi baris
+    // Prekondisi: Matriks persegi
+    public void detRowReduction() {
+
+    }
+
+    // Menghitung determinan matriks dengan metode ekspansi kofaktor
+    // Prekondisi: Matriks persegi
+    public float detCofactor() {
+        if (M == 0)
+        {
+            return 0;
+        }
+        else if (M == 1)
+        {
+            return Mat[0][0];
+        }
+        else
+        {
+            int i, j, k, tempKol;
+            float cofactor;
+            float det = 0;
+            int sign = 1;
+    
+            for (k = 0; k < N; k++)
+            {
+                Matriks temp = new Matriks(M-1, N-1);
+                cofactor = sign*Mat[0][k];
+                for (i = 1; i < M; i++)
+                {
+                    tempKol = 0;
+                    for (j = 0; j < N; j++)
+                    {
+                        if (j != k)
+                        {
+                            temp.Mat[i-1][tempKol] = this.Mat[i][j];
+                            tempKol++;
+                        }
+                    }
+                }
+                det += cofactor*temp.detCofactor();
+                sign *= (-1);
+            }
+            return det;
         }
     }
 }
