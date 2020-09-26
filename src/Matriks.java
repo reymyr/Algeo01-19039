491fee32a920854211a938dde6fa1a1073a68761
irvinandryan <irvin.andryan@gmail.com>
2020-09-26 22:09:23 +0700

    update metode row reduction

--- src/Matriks.java
+++ src/Matriks.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.Math;
 
 public class Matriks {
     int M;
@@ -99,25 +100,62 @@ public class Matriks {
             return Mat[0][0];
         }
         else { // Reduksi baris, membentuk matriks segitiga atas
-            int i,j,k;
+            int i,j,k,p;
             float det = 1;
             float x;
+            int jumlahSwap = 0;
+            int adaNol = 0;
+            int jumlahNol = 0;
+            int bar = 0;
+            int kol = 0;
 
-            for (i = 0; i < M; i++) {
-                for (j = 0; j < N; j++){
-                    if (i < j){
-                        x = Mat[j][i] / Mat[i][i];
-                        for (k = 0; k < M; k++){
-                            Mat[j][k] = Mat[j][k] - (Mat[i][k] * x);
-                        }
+            //cek apakah ada 0 di baris pertama
+            while (adaNol == 0 && kol < M) {
+                if (Mat[bar][kol] == 0) {
+                    adaNol = 1;
+                }
+                else{
+                    kol = kol + 1;
+                }
+            }
+            //jika ada 0, cek apakah kolom tersebut isinya 0 semua, jika ya (jumlahNol == M), maka det = 0
+            if(adaNol == 1){
+                for (bar = 0; bar < M; bar++){
+                    if(Mat[bar][kol] == 0){
+                        jumlahNol = jumlahNol + 1;
                     }
                 }
-                det = det * Mat[i][i];
             }
-            return det;
+            if(jumlahNol == M){
+                return 0;
+            }
+            //jika tidak ada kolom yang isinya 0 semua, lanjutkan dengan reduksi baris
+            else{
+                for (i = 0; i < M; i++) {
+                    for (j = 0; j < N; j++){
+                        if (i < j){
+                            if (Mat[i][i] == 0 && i != (M-1)){ 
+                                float temp;
+                                jumlahSwap = jumlahSwap + 1;
+                                for(p = 0; p < M; p++){
+                                    temp = Mat[i][p];
+                                    Mat[i][p] = Mat[M-1][p];
+                                    Mat[M-1][p] = temp;
+                                }
+                            }
+                            x = Mat[j][i] / Mat[i][i];
+                            for (k = 0; k < M; k++){
+                                Mat[j][k] = Mat[j][k] - (Mat[i][k] * x);
+                            }
+                        }
+                    }
+                    det = det * Mat[i][i];
+                }
+                det = det * (Math.pow(-1, jumlahSwap));
+                return det;
+            }
         }
     }
-
     // Menghitung determinan matriks dengan metode ekspansi kofaktor
     // Prekondisi: Matriks persegi
     public float detCofactor() {
