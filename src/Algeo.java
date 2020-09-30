e161546e17da2302cc34fadcd77776813ffe4890
rayfazt <rayfazt@gmail.com>
2020-10-01 02:49:55 +0700

    kelar plis kelar

--- src/Algeo.java
+++ src/Algeo.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.io.File;
 
 public class Algeo {
     public static void main(String[] args) {
@@ -9,72 +8,39 @@ public class Algeo {
                             + "3. Matriks balikan\n"
                             + "4. Interpolasi Polinom\n"
                             + "5. Regresi linier berganda\n"
-                            + "6. Keluar\n");
+                            + "6. Keluar");
         int choice;
         System.out.print("Pilihan: ");
         Scanner in = new Scanner(System.in);
         choice = Integer.parseInt(in.nextLine());
         switch (choice) {
             case 1:
-                int m, n, i, j, choiceInput;
-                Matriks mat;
-                System.out.println("1. Input dari keyboard");
-                System.out.println("2. Input dari file"); 
-                System.out.print("Pilihan (1/2): ");
-                choiceInput = Integer.parseInt(in.nextLine());
-                if (choiceInput == 1) {
-                    System.out.print("m: ");
-                    m = in.nextInt();
-                    System.out.print("n: ");
-                    n = in.nextInt();
-                    double[][] A = new double[m][n+1];
-                    for(i = 0; i < m; i++) {
-                        for (j = 0; j < n+1; j++) {
-                            if (j != n) {
-                                System.out.print("Koefisien a"+(i+1)+(j+1)+": ");
-                            }
-                            else {
-                                System.out.print("Konstanta b"+(i+1)+": ");
-                            }
-                            A[i][j] = in.nextDouble();
-                        }
-                    }
-                    mat = new Matriks(A);
-                }
-                else if (choiceInput == 2) {
-                    System.out.print("Nama file (tanpa .txt): ");
-                    String filename = in.nextLine();
-                    File inputFile = new File("../test/"+filename+".txt");
-                    mat = new Matriks(inputFile);
-                }
-                else {
-                    System.out.println("Input tidak valid");
-                    break;
-                }
-                
-                System.out.println("\nSisten Persamaan Linier\n"
+                System.out.println();
+                SPLSolver spl = new SPLSolver();
+                System.out.println();
+                System.out.println("Sisten Persamaan Linier\n"
                                     + "1. Metode eliminasi Gauss\n"
                                     + "2. Metode eliminasi Gauss-Jordan\n"
                                     + "3. Metode matriks balikan\n"
-                                    + "4. Kaidah Cramer\n");
+                                    + "4. Kaidah Cramer");
                 int choiceSPL;
                 System.out.print("Pilihan: ");
                 choiceSPL = Integer.parseInt(in.nextLine());    
                 switch (choiceSPL) {
                     case 1:
-                        SPLSolver.gauss(mat);
+                        spl.gauss();
                         break;
 
                     case 2:
-                        SPLSolver.gaussJordan(mat);
+                        spl.gaussJordan();
                         break;
 
                     case 3:
-                        SPLSolver.inverse(mat);
+                        spl.inverse();
                         break;
 
                     case 4:
-                        SPLSolver.cramer(mat);
+                        spl.cramer();
                         break;
                 
                     default:
@@ -83,19 +49,22 @@ public class Algeo {
                 }
                 break;
             case 2:
+                System.out.println();
+                DeterminanSolver detSolver = new DeterminanSolver();
+                System.out.println();
                 System.out.println("Determinan\n"
                                     + "1. Metode reduksi baris\n"
-                                    + "2. Metode ekspansi kofaktor\n");
+                                    + "2. Metode ekspansi kofaktor");
                 int choiceDet;
-                System.out.print("Pilihan (1/2): ");
-                choiceDet = Integer.parseInt(in.nextLine());    
-                switch (choiceDet) {
+                System.out.print("Pilihan: ");
+                choiceDet = Integer.parseInt(in.nextLine());  
+                switch (choiceDet){
                     case 1:
-
+                        detSolver.solveRowRed();
                         break;
 
                     case 2:
-
+                        detSolver.solveCofactor();
                         break;
 
                     default:
@@ -105,45 +74,50 @@ public class Algeo {
                 break;
                 
             case 3:
+                System.out.println();
+                InverseSolver invSolver = new InverseSolver();
+                System.out.println();
                 System.out.println("Matriks balikan\n"
                                     + "1. Metode kofaktor\n"
-                                    + "2. Metode Gauss-Jordan\n");
+                                    + "2. Metode Gauss-Jordan");
                 int choiceInv;
-                System.out.print("Pilihan (1/2): ");
+                System.out.print("Pilihan: ");
                 choiceInv = Integer.parseInt(in.nextLine());    
                 switch (choiceInv) {
                     case 1:
-
+                        invSolver.solveCofactor();
                         break;
 
                     case 2:
-
+                        invSolver.solveGaussJordan();
                         break;
 
                     default:
                         System.out.println("Input pilihan tidak valid");
                         break;
-                    }
+                }
                 break;
 
             case 4:
+                System.out.println();
                 Interpolasi interp = new Interpolasi();
                 interp.solve();
                 break;
 
             case 5:
-            
+                System.out.println();
+                LinearRegression LR = new LinearRegression();
+                LR.LRSolve();
                 break;
 
             case 6:
-            
+                System.out.println("Thank you and goodbye! Ciao");
                 break;
 
             default:
                 System.out.println("Input pilihan tidak valid");
                 break;
         }
-
         in.close();
     }
 }
--- src/LinearRegression.java
+++ src/LinearRegression.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 public class LinearRegression {
     Matriks mat;
     double[] xk;
+    boolean toFile;
 
     public LinearRegression() {
         int choiceInput;
@@ -23,6 +24,7 @@ public class LinearRegression {
             n = in.nextInt();
             System.out.print("k: ");
             col = in.nextInt();
+            this.xk = new double[col];
             double[][] A = new double[n][col+1];
             for (int i = 0; i < n; i++){
                 for (int j = 0; j < col+1; j++){
@@ -35,7 +37,18 @@ public class LinearRegression {
                     A[i][j] = in.nextDouble();
                 }
             }
-            mat = new Matriks(A);  
+            this.mat = new Matriks(col+1, col+2);
+            System.out.println(mat.M + " "+mat.N);
+            for (int i = 0; i < mat.M ;i++) {
+                for (int j = 0; j < mat.N; j++) {
+                    double el = 0;
+                    for (int k = 0; k < n; k++) {
+                        el += (j == 0 ? 1 : A[k][j-1]) * (i == 0 ? 1 : A[k][i-1]);
+                    }
+                    this.mat.Mat[i][j] = el;
+                }
+            }
+            this.mat.printMatriks();
         }
         else if (choiceInput == 2) {
             JFrame frame = new JFrame();
@@ -44,7 +57,6 @@ public class LinearRegression {
             if (fileChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
                 File inputFile = fileChooser.getSelectedFile();
                 try {
-                    int i, j;
                     int rows = 0, cols = 0;
                     String row = "";
                     Scanner rowScanner = new Scanner(inputFile);
@@ -61,24 +73,28 @@ public class LinearRegression {
                     colScanner.close();
                     double[][] A = new double[rows][cols];
                     Scanner fileScanner = new Scanner(inputFile);
-                    for(i = 0; i < rows; i++) {
-                        for (j = 0; j < cols; j++) {
+                    for(int i = 0; i < rows; i++) {
+                        for (int j = 0; j < cols; j++) {
                             A[i][j] = fileScanner.nextDouble();
                         }
                     }
                     fileScanner.close();
-    
+                    this.xk = new double[cols-1];
                     this.mat = new Matriks(cols, cols+1);
                     for (int i = 0; i < mat.M ;i++) {
-                        for (int j = 0; j < mat.N-1; j++) {
+                        for (int j = 0; j < mat.N; j++) {
                             double el = 0;
                             for (int k = 0; k < rows; k++) {
-                                el += (j == 0 ? 1 : A[k][j]) * (i == 0 ? 1 : A[k][i]);
+                                if (j < mat.N-1) {
+                                    el += (j == 0 ? 1 : A[k][j]) * (i == 0 ? 1 : A[k][i]);
+                                }
+                                else {
+                                    el += A[k][0] * (i == 0 ? 1 : A[k][i]);
+                                }
                             }
                             this.mat.Mat[i][j] = el;
                         }
                     }
-                    this.mat.printMatriks();
                     frame.dispose();
                 } catch (FileNotFoundException e) {
                     System.out.println("File not found.");
@@ -89,9 +105,9 @@ public class LinearRegression {
                 System.out.println("No file selected");
                 System.exit(0);
             }
-            this.xk = new double[col];
-            for (int i = 0 ; i < col; i++){
-                System.out.print("Masukkan xk" + (i + 1) + ": ");
+            
+            for (int i = 0 ; i < xk.length; i++){
+                System.out.print("Masukkan x" + (i + 1) + ": ");
                 this.xk[i] = in.nextDouble();
             }
         }
@@ -103,6 +119,96 @@ public class LinearRegression {
     }
 
     public void LRSolve() {
-        
+        this.mat.gauss();
+        double[] solutions = new double[this.mat.N-1];
+        for (int i = this.mat.N-2; i >= 0; i--) {
+            solutions[i] = this.mat.Mat[i][this.mat.N-1];
+
+            for (int j = i+1; j < this.mat.N-1; j++) {
+                solutions[i] -= this.mat.Mat[i][j]*solutions[j];
+            }
+        }
+        Scanner in = new Scanner(System.in);
+        System.out.println();
+        System.out.println("1. Output ke layar");
+        System.out.println("2. Output ke file"); 
+        System.out.print("Pilihan: ");
+        int choiceOutput;
+        choiceOutput = Integer.parseInt(in.nextLine());
+        switch (choiceOutput) {
+            case 1:
+                this.toFile = false;
+                break;
+            case 2:
+                this.toFile = true;
+                break;
+            default:
+                System.out.println("Input tidak valid");
+                break;
+        }
+        if (this.toFile) {
+            JFrame frame = new JFrame();
+            JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+            if (fileChooser.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                try {
+                    FileWriter writer = new FileWriter(file);
+                    writer.write("Persamaan regresi:\n");
+                    double res = 0;
+                    for (int i = 0; i < solutions.length; i++) {
+                        res += solutions[i]*(i == 0 ? 1 : xk[i-1]);
+                        if (solutions[i] < 0 && i == 0) {
+                            writer.write("-");
+                        }
+                        writer.write(String.format("%.4f", Math.abs(solutions[i])));
+                        if (i > 0) {
+                            writer.write("x"+i);
+                        }
+                        if (i < solutions.length-1) {
+                            if (solutions[i+1] >= 0) {
+                                writer.write(" + ");
+                            }
+                            else {
+                                writer.write(" - ");
+                            }
+                        }
+                    }
+                    writer.write("\n\n");
+                    writer.write("Nilai taksiran: "+String.format("%.4f", res));
+                    writer.close();
+                    frame.dispose();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else {
+                System.out.println("No file selected");
+                System.exit(0);
+            }
+        }
+        else {
+            System.out.println("Persamaan regresi:");
+            double res = 0;
+            for (int i = 0; i < solutions.length; i++) {
+                res += solutions[i]*(i == 0 ? 1 : xk[i-1]);
+                if (solutions[i] < 0 && i == 0) {
+                    System.out.print("-");
+                }
+                System.out.print(String.format("%.4f", Math.abs(solutions[i])));
+                if (i > 0) {
+                    System.out.print("x"+i);
+                }
+                if (i < solutions.length-1) {
+                    if (solutions[i+1] >= 0) {
+                        System.out.print(" + ");
+                    }
+                    else {
+                        System.out.print(" - ");
+                    }
+                }
+            }
+            System.out.println("\n");
+            System.out.println("Nilai taksiran: "+String.format("%.4f", res));
+        }
     }
 }
